5a80a64ae382ef3786cc544c638d5166eef19171
Marcel Luethi <marcel.luethi@unibas.ch>
2016-07-30 22:30:51 +0200

    implement create group

--- ui.thrift
+++ ui.thrift
@@ -1,13 +1,30 @@
 
 namespace cpp ui // The generated c++ code will be put inside example namespace
 
-struct Point {
+struct Group {
+    1: required i32 id;
+    2: required string name;
+}
+
+struct Point3D {
    1: required double x;
    2: required double y;
    3: required double z;
 }
 
-typedef list<Point> PointList
+struct Vector3D {
+    1: required double x;
+    2: required double y;
+    3: required double z;
+}
+
+struct IntVector3D {
+    1: required i32 i;
+    2: required i32 j;
+    3: required i32 k;
+}
+
+typedef list<Point3D> PointList
 
 
 struct TriangleCell {
@@ -16,16 +33,33 @@ struct TriangleCell {
     3: required i32 id3;
 }
 
+
 typedef list<TriangleCell> TriangleCellList
+
 
 struct TriangleMesh {
     1: required PointList vertices;
     2: required TriangleCellList topology;
 }
 
+struct ImageDomain {
+    1: required Point3D origin;
+    2: required IntVector3D size;
+    3: required Vector3D spacing;
+}
+
+typedef list<i16> ImageData
+
+struct Image {
+    1: required ImageDomain domain;
+    2: required ImageData data;
+}
+
 service UI {
-  void showPointCloud(1:PointList p);
-  void showTriangleMesh(1:TriangleMesh m);
+  Group createGroup(1:string name);
+  void showPointCloud(1: Group g, 2:PointList p, 3:string name);
+  void showTriangleMesh(1: Group g, 2:TriangleMesh m, 3:string name);
+  void showImage(1: Group g, 2:Image img, 3:string name);
 }
 
 
